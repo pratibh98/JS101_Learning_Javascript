9e9eab2be00d49bd4acabaee39683b4049cd2df5
pratibh98 <pratibhrs@gmail.com>
2023-02-06 13:34:44 +0530

    Mathematical Operator

---
+++ Day - 02 - Programs/Problem1.js
@@ -0,0 +1,9 @@
+//Problem 1: If the number is divisible by 3, print a "multiple of 3".
+
+let num = 30;
+
+if ((num%3)==0) {
+  console.log("Multiple of 3");
+}else{
+  console.log("Not Multiple of 3");
+}
