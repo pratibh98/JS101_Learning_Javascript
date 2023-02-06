e024c3b644a759561f78237c14ff10e523217233
pratibh98 <123896529+pratibh98@users.noreply.github.com>
2023-02-06 13:34:03 +0530

    Delete Day - 02 - Programs directory

--- Day - 02 - Programs/Problem2.js
+++
@@ -1,9 +0,0 @@
-//Problem 2: If a person is allowed to drive in India print "Apply for a license" or "NA".
-
-let min_age = 25;
-
-if(min_age>=18){
-  console.log("Person can apply for a license!");
-}else{
-  console.log("NA");
-}
