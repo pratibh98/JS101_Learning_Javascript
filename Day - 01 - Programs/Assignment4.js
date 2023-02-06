97a2a525ecc5f47e65bde77c55f52b9c8ca37155
pratibh98 <pratibhrs@gmail.com>
2023-02-06 13:27:07 +0530

    Introduction to JS(variables)

---
+++ Day - 01 - Programs/Assignment4.js
@@ -0,0 +1,27 @@
+/* Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+Print the report card of the student (You can make it look nice by using some keyboard symbols )
+Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration */
+
+let name= "Itachi";
+let school= "DPS";
+let grade= "A";
+let section= "12 D";
+let rollno= 20;
+let Maths = 65;
+let Physics = 80;
+let Chemistry = 78;
+
+console.log("\t\t尺🝗尸ㄖ尺セ ⼕闩尺ᗪ");
+
+console.log("\n\t𝗦𝘁𝘂𝗱𝗲𝗻𝘁 𝗗𝗲𝘁𝗮𝗶𝗹𝘀 ");
+console.log("Name :- ",name);
+console.log("Rollno :- ",rollno);
+console.log("Section :- ",section);
+console.log("School :- ",school);
+console.log("Grade :- ",grade);
+
+console.log("\n\t𝗠𝗮𝗿𝗸𝘀 𝗦𝗰𝗼𝗿𝗲𝗱");
+console.log("Maths :-\t",Maths);
+console.log("Physics :-\t",Physics);
+console.log("Chemistry :-",Chemistry);
+ 
